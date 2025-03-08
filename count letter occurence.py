9db2b5ec6febc09504cc934b6c6c5a94aef9e116
nsnangal <nsnangal@gmail.comgit config --global user.name>
2025-03-08 22:20:46 +0530

    phonebook.py

---
+++ count letter occurence.py
@@ -0,0 +1,11 @@
+from collections import defaultdict
+def word_counts(sentense):
+    fsentence=sentense.lower().strip(".,?!")
+    word_dic={}
+    for word in fsentence.replace(" ",""):
+        word_dic[word]=word_dic.get(word,0)
+    print(word_dic)
+
+sentence=input("enter your sentence\n")
+word_counts(sentence)
+
